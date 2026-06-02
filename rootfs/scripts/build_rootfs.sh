07582bc3fc83222de4994436bb505033dd4b9d27
xxxx2289 <139218008+xxxx2289@users.noreply.github.com>
2026-06-02 10:00:03 +0800

    Create build_rootfs.sh for Debian rootfs setup
    Add a script to build a Debian root filesystem using debootstrap.

--- rootfs/scripts/build_rootfs.sh
+++ rootfs/scripts/build_rootfs.sh
@@ -1,1 +1,24 @@
-ze
+#!/bin/bash
+set -e
+
+ROOTFS_DIR="../debian-rootfs"
+DEBIAN_VERSION="bookworm"
+ARCH="arm64"
+
+sudo apt update
+sudo apt install -y debootstrap qemu-user-static
+
+mkdir -p $ROOTFS_DIR
+
+sudo debootstrap \
+  --arch=$ARCH \
+  $DEBIAN_VERSION \
+  $ROOTFS_DIR \
+  http://deb.debian.org/debian
+
+# 清理
+sudo chroot $ROOTFS_DIR apt clean
+
+# 生成 initramfs（最简）
+cd $ROOTFS_DIR
+sudo find . | cpio -H newc -o | gzip > ../initramfs.cpio.gz
