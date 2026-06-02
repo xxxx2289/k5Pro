4ac2ddce92b1ce07f61f4cf384687acdd7de4405
xxxx2289 <139218008+xxxx2289@users.noreply.github.com>
2026-06-02 10:00:49 +0800

    Add shebang and set options in mkbootimg.sh

--- bootimg/mkbootimg.sh
+++ bootimg/mkbootimg.sh
@@ -1,1 +1,11 @@
-ze
+#!/bin/bash
+set -e
+
+KERNEL="../kernel/out/Image.gz-dtb"
+RAMDISK="../debian-rootfs/initramfs.cpio.gz"
+OUTPUT="../K5Pro_Linux_boot.img"
+
+mkbootimg \
+  --kernel $KERNEL \
+  --ramdisk $RAMDISK \
+  --output $OUTPUT
