583ce1cfd271920433da3227f228f5cb22986235
xxxx2289 <139218008+xxxx2289@users.noreply.github.com>
2026-06-02 15:10:13 +0800

    Update mkbootimg.sh

--- bootimg/mkbootimg.sh
+++ bootimg/mkbootimg.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -e
 
-KERNEL="../kernel/out/Image.gz-dtb"
-RAMDISK="../rootfs/initramfs.cpio.gz"
-OUTPUT="../K5Pro_Linux_boot.img"
+KERNEL=kernel/out/Image.gz-dtb
+RAMDISK=rootfs/initramfs.cpio.gz
+OUT=K5Pro_Ubuntu22.04_boot.img
 
 mkbootimg \
   --kernel $KERNEL \
   --ramdisk $RAMDISK \
-  --output $OUTPUT
+  --output $OUT
