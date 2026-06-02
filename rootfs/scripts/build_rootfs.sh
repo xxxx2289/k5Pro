72f6a912ec31dfb80e3badd5b279442e32ec0e61
xxxx2289 <139218008+xxxx2289@users.noreply.github.com>
2026-06-02 15:17:08 +0800

    Update build_rootfs.sh

--- rootfs/scripts/build_rootfs.sh
+++ rootfs/scripts/build_rootfs.sh
@@ -2,18 +2,27 @@
 set -e
 
 ROOTFS_DIR="../rootfs/ubuntu"
+INITRAMFS_DIR="../rootfs/initramfs"
 ARCH=arm64
 UBUNTU_VERSION=jammy
 
-mkdir -p $ROOTFS_DIR
+mkdir -p $ROOTFS_DIR $INITRAMFS_DIR
 
-# 安装 debootstrap
-sudo apt update
-sudo apt install -y debootstrap qemu-user-static cpio
-
-# 构建最小 Ubuntu 22.04 rootfs
+# 构建最小 Ubuntu rootfs
 sudo debootstrap --arch=$ARCH $UBUNTU_VERSION $ROOTFS_DIR http://ports.ubuntu.com/ubuntu-ports
 
-# 生成 initramfs
-cd $ROOTFS_DIR
+# 创建 init 脚本，作为 Linux 启动桥
+cat << 'EOF' | sudo tee $INITRAMFS_DIR/init
+#!/bin/sh
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+mount -t devtmpfs devtmpfs /dev
+echo "Booting Ubuntu on K5 Pro..."
+exec /sbin/init
+EOF
+
+sudo chmod +x $INITRAMFS_DIR/init
+
+# 打包 initramfs
+cd $INITRAMFS_DIR
 sudo find . | cpio -H newc -o | gzip > ../initramfs.cpio.gz
